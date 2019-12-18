9226c408f359ac91af665e2be3f359d4dda910d4
陈午阳 <wuyang.chen@wedaoyi.com>
2019-12-18 15:55:10 +0800

    first commit

---
+++ WYPageController/WYPageBaseController.h
@@ -0,0 +1,17 @@
+//
+//  WYPageBaseController.h
+//  WYPageController
+//
+//  Created by 陈午阳 on 2019/12/18.
+//  Copyright © 2019 陈午阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WYPageBaseController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WYPageController/WYPageController.m
@@ -0,0 +1,32 @@
+//
+//  WYPageController.m
+//  WYPageController
+//
+//  Created by 陈午阳 on 2019/12/18.
+//  Copyright © 2019 陈午阳. All rights reserved.
+//
+
+#import "WYPageController.h"
+
+@interface WYPageController ()
+
+@end
+
+@implementation WYPageController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
